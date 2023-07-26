85f225dd6995e630b53b8b53abfcdf5d48ece4c8
Linf0rd <linfordsnagger@gmx.com>
2023-07-26 05:32:42 +0200

    Add read_line function
    Create a read_line function in the shell.h file. This function is responsible for reading a line of input from the user on the standard input (stdin). It uses the getline function to dynamically allocate memory for the input line. The function returns the line that was entered by the user as a char pointer. If the getline function encounters an error or reaches the end of the input (EOF), the function handles the cases accordingly. If EOF is encountered, it writes a new line character to the standard output and exits the Shell successfully with an exit status of 0. If any other error occurs during reading, it prints an error message to the standard error (perror) and exits the Shell with a failure exit status (1). This function enhances the user experience by providing a convenient way to read input lines in the Shell.

---
+++ read_line.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+/**
+ * read_line - Read a line from the user.
+ *
+ * Return: The line that was entered by the user.
+ */
+
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
+
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			write(STDOUT_FILENO, "\n", 1);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("#cisfun$");
+			exit(EXIT_FAILURE);
+		}
+	}
+	return (line);
+}
