e271787079f6102e4153452407feda7860d2cf9b
Linf0rd <linfordsnagger@gmx.com>
2023-07-26 05:28:52 +0200

    Add shell.h header file
    Create the shell.h header file, which contains function prototypes and includes necessary standard library headers for the Simple Shell. The header file guards (ifndef, define, endif) are used to prevent multiple inclusions of the same file. The shell.h file declares the following functions: split_line, prompt, execute, free_args, and read_line. The split_line function is responsible for splitting a line of input into individual tokens based on a specified delimiter. The prompt function displays the Shell prompt to the user. The execute function is used to execute a command with its arguments. The free_args function releases the memory allocated for the array of arguments. The read_line function reads a line from the user and returns the entered line. The shell.h header file is an essential part of the Shell program as it provides the function prototypes necessary for the Shell's functionality.

---
+++ README.md
@@ -0,0 +1,4 @@
+** Simple Shell **
+
+This is a simple Unix Shell implementation written in C. It provides basic functionality to execute commands and display a prompt for the user to interact with the Shell.
+
---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+char **split_line(char *line, char *delim);
+void prompt(void);
+int execute(char **args);
+void free_args(char **args);
+char *read_line(void);
+
+#endif /* SHELL_H */
